7054f462b3958fc0d851f1f4916e53bc810cde44
sarvagyasinghh <sarvagyasinghh@gmail.com>
2026-03-26 21:47:25 +0530

    Fix navbar icon imports

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '@/context/AuthContext';
 import { useTheme } from '@/context/ThemeContext';
-import { FaSearch, FaBell, FaUser, FaMoon, FaSun } from 'react-icons/fa';
+import { FaSearch, FaBell, FaUser, FaMoon, FaSun, FaTimes, FaMenu } from 'react-icons/fa';
 import { ROUTES, USER_ROLES } from '@/lib/constants';
 import styles from './Navbar.module.css';
 
